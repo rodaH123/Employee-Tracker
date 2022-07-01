03253720c70dbfdd20bbaaee0fa1fecaa9111667
Roro <100329632+rodaH123@users.noreply.github.com>
2022-07-01 12:12:53 -0500

    updated schema file

---
+++ server.js
@@ -0,0 +1,18 @@
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+  
+// call once somewhere in the beginning of the app
+  const cTable = require('console.table');
